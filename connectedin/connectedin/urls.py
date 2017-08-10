350857111a015596329511bf34ff633240e354ae
Marcelo Silva <desenv.marcelo.silva@gmail.com>
2017-08-09 22:44:10 -0300

    Ajustando as rotas separando para que cada rota fique na sua aplicação dentro do projeto.

--- connectedin/connectedin/urls.py
+++ connectedin/connectedin/urls.py
@@ -7,5 +7,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'perfis.views.index'),
+    url(r'^', include('perfis.urls')),
 )
--- connectedin/perfis/views.py
+++ connectedin/perfis/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Bem vindo ao projeto.")
+    return HttpResponse("Bem vindo ao projeto ConnectedIn.")
