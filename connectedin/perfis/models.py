3305afc1981b32538c44cd019a360d7e6456e8ae
Marcelo Silva <desenv.marcelo.silva@gmail.com>
2017-08-10 22:29:23 -0300

    Criação de migração. Foram inseridos dados através do shell.

--- connectedin/perfis/models.py
+++ connectedin/perfis/models.py
@@ -1,9 +1,18 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 
+# usar python manage.py makemigrations para criar o esquema de migração (ver pasta migrate)
 
-class Perfil(object):
-    def __init__(self, nome='', email='', telefone='', empresa=''):
-        self.nome = nome
-        self.email = email
-        self.telefone = telefone
-        self.empresa = empresa
+# usar python manage.py migrate para gerar o resultado deste migration no banco
+
+# usar python manage.py shell para abrir um console onde pode-se instanciar e
+# fazer operações como salvar dados nesta classe
+
+#pode-se usar o comando python manage.py sqlmigrate [nome do app (aqui é perfis)] e o nome da migrate
+#iso vai retornar os comandos SQL utilizados para rodar essa migração.
+
+class Perfil(models.Model):
+    nome = models.CharField(max_length=255, null=False)
+    email = models.CharField(max_length=255, null=False)
+    telefone = models.CharField(max_length=15, null=False)
+    empresa = models.CharField(max_length=255, null=False)
--- connectedin/perfis/views.py
+++ connectedin/perfis/views.py
@@ -1,15 +1,9 @@
 from django.shortcuts import render
 from perfis.models import Perfil
+
 def index(request):
     return render(request, 'index.html')
 
 def exibir(request, perfil_id):
-    perfil = Perfil()
-
-    if perfil_id == '1':
-        perfil = Perfil('Marcelo Silva', 'mlsilva@gmail.com', '(12) 9 9121-6615', 'PrimaSoft')
-
-    if perfil_id == '2':
-        perfil = Perfil('Lucia Lopes', 'lucia@gmail.com', '(12) 9 9722-1515', 'CTA/DPC')
-
+    perfil = Perfil.objects.get(id=perfil_id)
     return render(request, 'perfil.html', {'perfil': perfil})
