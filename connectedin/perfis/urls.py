32b40113000dd08c7349bb85e93d26b65cbf1dfa
Marcelo Silva <desenv.marcelo.silva@gmail.com>
2017-08-10 21:31:37 -0300

    Criação da exibição de perfis. Acesso usando URL com números sem acessar o valor.

--- connectedin/perfis/urls.py
+++ connectedin/perfis/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, url
+from perfis.views import index, exibir
 
 urlpatterns = patterns('',
-    url(r'^$', 'perfis.views.index')
+    url(r'^$', index, name='index'),
+    url(r'^perfis/\d+$', exibir, name='exibir')
 )
