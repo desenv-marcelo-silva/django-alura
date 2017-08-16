84a6225fc6a07b5a26f20e118ae53619a88b4a79
Marcelo Silva <desenv.marcelo.silva@gmail.com>
2017-08-15 22:45:28 -0300

    Adicionar o bootstrap. Criando templates base e nova pasta static para o Django prover esse conteúdo.

--- connectedin/perfis/urls.py
+++ connectedin/perfis/urls.py
@@ -4,5 +4,5 @@ from perfis import views
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^perfis/(?P<perfil_id>\d+)$', views.exibir, name='exibir'),
-    url(r'^perfis/(?P<perfil_id>\d+)/convidar$', views.convidar, name='exibir')
+    url(r'^perfis/(?P<perfil_id>\d+)/convidar$', views.convidar, name='convidar')
 )
